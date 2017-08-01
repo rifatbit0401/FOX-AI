d02895ac99fae1fe098e54e3370aef624f5e9a65
rifatbit0401 <bit0401@iit.du.ac.bd>
2017-08-01 00:32:33 -0700

    delete jackson 2.2.4.jar and added 1.9.0.jar, and expose json file writer service

--- src/com/mcc/rifat/main/Main.java
+++ src/com/mcc/rifat/main/Main.java
@@ -1,10 +1,29 @@
 package com.mcc.rifat.main;
 
+import com.mcc.rifat.model.Conversation;
+import com.mcc.rifat.utility.JsonService;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args){
 
         String conversationJsonFilePath = "";
+        File jsonFile = new File("/home/rifat/Desktop/Data/test.json");
+        Conversation conversation = new Conversation();
+        conversation.dialogues.add("d1");
+        conversation.dialogues.add("d2");
+
+        List<Conversation>conversations = new ArrayList<Conversation>();
+        conversations.add(conversation);
+        conversations.add(conversation);
+        JsonService jsonService = new JsonService();
+        jsonService.writeJsonFile(jsonFile,conversations);
+
+
 
     }
 }
--- src/com/mcc/rifat/utility/JsonService.java
+++ src/com/mcc/rifat/utility/JsonService.java
@@ -1,4 +1,22 @@
 package com.mcc.rifat.utility;
 
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.codehaus.jackson.util.DefaultPrettyPrinter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class JsonService {
+
+    public void writeJsonFile(File jsonFile, Object object){
+
+        ObjectWriter objectWriter = new ObjectMapper().writer(new DefaultPrettyPrinter());
+        try {
+            objectWriter.writeValue(jsonFile,object);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
